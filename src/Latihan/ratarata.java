dbf35d935b4d0cd490f341778951aa804aef329e
hp <ekowibowo030997@gmail.com>
2021-06-10 11:27:30 +0700

    file pertemuan 9

---
+++ src/Latihan/ratarata.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author hp
+ */
+public class ratarata {
+    public double nilai1,nilai2,ratarata;
+
+    public double getNilai1() {
+        return nilai1;
+    }
+
+    public void setNilai1(double nilai1) {
+        this.nilai1 = nilai1;
+    }
+
+    public double getNilai2() {
+        return nilai2;
+    }
+
+    public void setNilai2(double nilai2) {
+        this.nilai2 = nilai2;
+    }
+
+    public double getRatarata() {
+        return ((nilai1+nilai2)/2);
+    }
+
+    }
+
+    
+   
+
